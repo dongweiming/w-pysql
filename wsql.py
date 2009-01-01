61de7f7026b81346d01498a26f975902450f0fbb
GuozhuWen <GuozhuWen@6ffbce92-d6e6-11dd-86c2-0f8696b7b6f9>
2009-01-01 08:59:09 +0000

    fixed the Chinese print width

--- wsql.py
+++ wsql.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os, sys
+import re
 import MySQLdb
 import MySQLdb.cursors
+import traceback, cStringIO
 pwd = os.path.dirname(os.path.realpath(sys.argv[0]))
 per = os.path.dirname(pwd)
 sys.path.append(per)
@@ -12,6 +14,60 @@ from sqlstore import *
 MAX_LIMIT = 10
 mode = 'sql'
 tmpfile = '/tmp/wsql.tmp'
+
+regx = re.compile(u"([\u2e80-\uffff])", re.UNICODE)
+
+def strException():
+    sio = cStringIO.StringIO()
+    traceback.print_exc(file=sio)
+    s = sio.getvalue()
+    sio.close()
+    return s
+
+def process_py(cmd):
+    eval(cmd)
+
+def process_sql(store, cmd):
+    try:
+        print cmd
+        store.farm.execute(cmd)
+        desc = store.farm.description
+        keys = [item[0] for item in desc]
+        content = store.farm.fetchall()
+        if not content:
+            print 'null result'
+            return
+        if len(content) > MAX_LIMIT: content = content[:MAX_LIMIT]
+        for row in content:
+            for k, v in row.items():
+                #v = regx.sub(r'\1\0', str(v).decode('utf-8'))
+                row[k] = str(v).decode('utf-8')
+        key_len = {}
+        for k in keys:
+            #vl = [len(str(row[k])) for row in content]
+            vl = [len(row[k]) for row in content]
+            vl.append(len(str(k)))
+            key_len[k] = max(vl)
+        format_str = '|'
+        for k in keys:
+            format_str += ' %%(%s)-%ss |'%(k,key_len[k])
+        ##print header
+        hr = '-'*(sum(key_len.values())+3*len(key_len)+1)
+        print hr
+        print format_str%dict(zip(keys,keys))
+        print hr
+        for row in content:
+            format_str = '|'
+            for k in keys:
+                ##the number of chinese words
+                num_cw = len(regx.findall(row[k]))
+                format_str += ' %%(%s)-%ss |'%(k,key_len[k]-num_cw)
+            outstr = format_str%row
+            print outstr
+        print hr
+    except:
+        e = strException()
+        print e
 
 def get_store(conf):
     return SqlStore(host=conf['host'],user=conf['user'],passwd=conf['passwd'],\
@@ -25,8 +81,10 @@ press sql command to query mysql.\n'''
     global MAX_LIMIT, mode
     store = get_store(luz2)
     while 1:
-        print '%s>'%mode,
-        cmd = raw_input().strip()
+        prompt = '%s>'%mode
+        #cmd = raw_input(prompt).strip()
+        cmd = raw_input(prompt)
+        if not cmd: continue
         if cmd == 'q':
             print 'exit'
             break
@@ -41,25 +99,10 @@ press sql command to query mysql.\n'''
         elif cmd[:4].lower() == 'limit ':
             MAX_LIMIT = cmd.split()[-1]
         else:
-            print cmd
-            store.farm.execute(cmd)
-            desc = store.farm.description
-            keys = [item[0] for item in desc]
-            content = store.farm.fetchall()
-            if not content:
-                print 'null result'
-            if len(content) > MAX_LIMIT: content = content[:MAX_LIMIT]
-            key_len = {}
-            for k in keys:
-                vl = [len(str(row[k])) for row in content]
-                vl.append(len(str(k)))
-                key_len[k] = max(vl)
-            format_str = ''
-            for k in keys:
-                format_str += '%%(%s)-%ss|'%(k,key_len[k]+3)
-            print format_str%dict(zip(keys,keys))
-            for row in content:
-                print format_str%row
+            if mode == 'py':
+                process_py(cmd)
+            elif mode == 'sql':
+                process_sql(store, cmd)
 
     store.close()
 
