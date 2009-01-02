ca7d9db7aea10f01ddba2c9368626a402f33680f
GuozhuWen <GuozhuWen@6ffbce92-d6e6-11dd-86c2-0f8696b7b6f9>
2009-01-02 15:47:48 +0000

    add multi-db selection

--- wsql.py
+++ wsql.py
@@ -24,60 +24,50 @@ def strException():
     sio.close()
     return s
 
-def process_py(cmd):
-    eval(cmd)
-
 def process_sql(store, cmd):
-    try:
-        print cmd
-        store.farm.execute(cmd)
-        desc = store.farm.description
-        keys = [item[0] for item in desc]
-        content = store.farm.fetchall()
-        if not content:
-            print 'null result'
-            return
-        if len(content) > MAX_LIMIT: content = content[:MAX_LIMIT]
-        for row in content:
-            for k, v in row.items():
-                #v = regx.sub(r'\1\0', str(v).decode('utf-8'))
-                row[k] = str(v).decode('utf-8')
-        key_len = {}
-        for k in keys:
-            #vl = [len(str(row[k])) for row in content]
-            vl = [len(row[k]) for row in content]
-            vl.append(len(str(k)))
-            key_len[k] = max(vl)
+    print cmd
+    store.farm.execute(cmd)
+    desc = store.farm.description
+    keys = [item[0] for item in desc]
+    content = store.farm.fetchall()
+    if not content:
+        print 'null result'
+        return
+    if len(content) > MAX_LIMIT: content = content[:MAX_LIMIT]
+    for row in content:
+        for k, v in row.items():
+            #v = regx.sub(r'\1\0', str(v).decode('utf-8'))
+            row[k] = str(v).decode('utf-8')
+    key_len = {}
+    for k in keys:
+        #vl = [len(str(row[k])) for row in content]
+        vl = [len(row[k]) for row in content]
+        vl.append(len(str(k)))
+        key_len[k] = max(vl)
+    format_str = '|'
+    for k in keys:
+        format_str += ' %%(%s)-%ss |'%(k,key_len[k])
+    ##print header
+    hr = '-'*(sum(key_len.values())+3*len(key_len)+1)
+    print hr
+    print format_str%dict(zip(keys,keys))
+    print hr
+    for row in content:
         format_str = '|'
         for k in keys:
-            format_str += ' %%(%s)-%ss |'%(k,key_len[k])
-        ##print header
-        hr = '-'*(sum(key_len.values())+3*len(key_len)+1)
-        print hr
-        print format_str%dict(zip(keys,keys))
-        print hr
-        for row in content:
-            format_str = '|'
-            for k in keys:
-                ##the number of chinese words
-                num_cw = len(regx.findall(row[k]))
-                format_str += ' %%(%s)-%ss |'%(k,key_len[k]-num_cw)
-            outstr = format_str%row
-            print outstr
-        print hr
-    except:
-        e = strException()
-        print e
+            ##the number of chinese words
+            num_cw = len(regx.findall(row[k]))
+            format_str += ' %%(%s)-%ss |'%(k,key_len[k]-num_cw)
+        outstr = format_str%row
+        print outstr
+    print hr
 
 def get_store(conf):
     return SqlStore(host=conf['host'],user=conf['user'],passwd=conf['passwd'],\
             db=conf['db'],port=conf['port'],cursorclass=MySQLdb.cursors.DictCursor)
 
 def cmd_loop():
-    print '''Welcome to wsql,
-press "q" to exit,
-press "use **" to switch database,
-press sql command to query mysql.\n'''
+    print '''Welcome to wsql, \npress "q" to exit, \npress "use **" to switch database, \npress sql command to query mysql.\n'''
     global MAX_LIMIT, mode
     store = get_store(luz2)
     while 1:
@@ -90,19 +80,27 @@ press sql command to query mysql.\n'''
             break
         elif cmd[:4].lower() == 'use ':
             db = cmd.split()[-1]
-            print db
             store.close()
-            store = get_store(eval(db))
+            if db[-1].isdigit():
+                dbconf = eval(db)
+            else:
+                dbconf = eval(db+'conf')
+            print 'DB config:', dbconf
+            store = get_store(dbconf)
         elif cmd == 'mode':
             if mode == 'sql': mode = 'py'
             else: mode='sql'
         elif cmd[:4].lower() == 'limit ':
             MAX_LIMIT = cmd.split()[-1]
         else:
-            if mode == 'py':
-                process_py(cmd)
-            elif mode == 'sql':
-                process_sql(store, cmd)
+            try:
+                if mode == 'py':
+                    exec cmd
+                elif mode == 'sql':
+                    process_sql(store, cmd)
+            except:
+                e = strException()
+                print e
 
     store.close()
 
